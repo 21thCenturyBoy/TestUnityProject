289150603b807dd80a5deb28eb602f11b258452d
21thCenturyBoy <2524755668@qq.com>
2023-04-17 17:18:50 +0800

    完善测试音量功能

--- Assets/TestAudioMixMgr/TestAudioMixMgr.cs
+++ Assets/TestAudioMixMgr/TestAudioMixMgr.cs
@@ -18,21 +18,28 @@ namespace TestAudioMix
             m_MasterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
             m_BGMVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
             m_EffectVolumeSlider.onValueChanged.AddListener(SetSoundEffectVolume);
+
+            SetMasterVolume(1);
+            SetMusicVolume(1);
+            SetSoundEffectVolume(1);
         }
 
         public void SetMasterVolume(float val) // 控制主音量的函数
         {
-            audioMixer.SetVolume("MasterVolume", val);
+            audioMixer.SetVolume("MasterVolume", -35, 0, val);
+            m_MasterVolumeSlider.transform.Find("Handle Slide Area/Handle/ValText").GetComponent<Text>().text = $"{val:P}";
         }
 
-        public void SetMusicVolume(float volume) // 控制背景音乐音量的函数
+        public void SetMusicVolume(float val) // 控制背景音乐音量的函数
         {
-            audioMixer.SetVolume("BGMVolume", volume);
+            audioMixer.SetVolume("BGMVolume", -35, 0, val);
+            m_BGMVolumeSlider.transform.Find("Handle Slide Area/Handle/ValText").GetComponent<Text>().text = $"{val:P}";
         }
 
-        public void SetSoundEffectVolume(float volume) // 控制音效音量的函数
+        public void SetSoundEffectVolume(float val) // 控制音效音量的函数
         {
-            audioMixer.SetVolume("EffectVolume", volume);
+            audioMixer.SetVolume("EffectVolume", -35, 0, val);
+            m_EffectVolumeSlider.transform.Find("Handle Slide Area/Handle/ValText").GetComponent<Text>().text = $"{val:P}";
         }
     }
     namespace UnityEngine.Audio
@@ -64,6 +71,40 @@ namespace TestAudioMix
                 }
 
                 return 0f;
+            }
+            public static void SetVolume(this AudioMixer mixer, string exposedName, float minVal, float maxVal, float value)
+            {
+                float newVal = Mathf.Clamp01(value);
+                if (value < 0.001f)
+                {
+                    value = 0f;
+                    mixer.SetFloat(exposedName, -80.0f);
+                }
+                else if (value > 0.999f)
+                {
+                    value = 1f;
+                    mixer.SetFloat(exposedName, maxVal);
+                }
+                else mixer.SetFloat(exposedName, Mathf.Lerp(minVal, maxVal, Mathf.Clamp01(value)));
+            }
+
+            public static float GetVolume(this AudioMixer mixer, string exposedName, float minVal, float maxVal, float value)
+            {
+                float val = 0f;
+                if (mixer.GetFloat(exposedName, out float volume))
+                {
+                    val = Mathf.InverseLerp(minVal, maxVal, volume);
+                    if (value < 0.001f)
+                    {
+                        value = 0f;
+                    }
+                    else if (value > 0.999f)
+                    {
+                        value = 1f;
+                    }
+                    return val;
+                }
+                else return 0f;
             }
         }
     }
