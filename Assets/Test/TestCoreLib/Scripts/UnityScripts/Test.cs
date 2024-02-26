53b434101d53811c2b1924711649c52826e098e8
21thCenturyBoy <2524755668@qq.com>
2024-02-26 20:42:59 +0800

    升级工程至2022.3.20f1

--- Assets/Test/TestCoreLib/Scripts/UnityScripts/Test.cs
+++ Assets/Test/TestCoreLib/Scripts/UnityScripts/Test.cs
@@ -5,48 +5,51 @@ using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
-public class Test : MonoBehaviour
-{
-    public GameObject cube;
-    public float interval, sum;
+/// <summary>
+///     "com.unity.entities": "0.51.1-preview.21", -->     "com.unity.entities": "1.0.16",
+/// </summary>
+//public class Test : MonoBehaviour
+//{
+//    public GameObject cube;
+//    public float interval, sum;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        GameObjectConversionSettings tempSettings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
-        Unity.Entities.Entity tempEntityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(cube, tempSettings);
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        GameObjectConversionSettings tempSettings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+//        Unity.Entities.Entity tempEntityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(cube, tempSettings);
 
-        EntityManager tempEntityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+//        EntityManager tempEntityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
 
-        Translation tempTranslation = new Translation();
-        tempTranslation.Value.x = -interval;
-        for (int i = 0; i < sum; i++)
-        {
-            for (int j = 0; j < sum; j++)
-            {
-                Unity.Entities.Entity tempCube = tempEntityManager.Instantiate(tempEntityPrefab);
-                tempTranslation.Value.x += interval;
-                tempEntityManager.SetComponentData(tempCube, tempTranslation);
-            }
+//        Translation tempTranslation = new Translation();
+//        tempTranslation.Value.x = -interval;
+//        for (int i = 0; i < sum; i++)
+//        {
+//            for (int j = 0; j < sum; j++)
+//            {
+//                Unity.Entities.Entity tempCube = tempEntityManager.Instantiate(tempEntityPrefab);
+//                tempTranslation.Value.x += interval;
+//                tempEntityManager.SetComponentData(tempCube, tempTranslation);
+//            }
 
-            tempTranslation.Value.x = -interval;
-            tempTranslation.Value.y += interval;
-        }
-    }
+//            tempTranslation.Value.x = -interval;
+//            tempTranslation.Value.y += interval;
+//        }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
 
-    }
-}
-public class TestTranslationSystem : ComponentSystem
-{
-    protected override void OnUpdate()
-    {
-        Entities.ForEach((ref Translation pTranslationComponentData1) =>
-        {
-            pTranslationComponentData1.Value = new float3(1, 1, 1);
-        });
-    }
-}
+//    }
+//}
+//public class TestTranslationSystem : ComponentSystem
+//{
+//    protected override void OnUpdate()
+//    {
+//        Entities.ForEach((ref Translation pTranslationComponentData1) =>
+//        {
+//            pTranslationComponentData1.Value = new float3(1, 1, 1);
+//        });
+//    }
+//}
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This is a backup of the my Unity test project 。Record problems encountered and
 本项目由[JetBranins](https://www.jetbrains.com/?from=TestUnityProject)赞助相关开发工具  
 <a href="https://www.jetbrains.com/?from=Unity3DTraining"><img src="https://github.com/21thCenturyBoy/TestUnityProject/blob/master/Docs/Images/jetbrains.png" width = "150" height = "150" div align=center /></a>
 
-## Unity Version ：2021.3.6f1
+## Unity Version ：2022.3.20f1
 
 ## Some interesting ideas：
 
