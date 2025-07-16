090e98bbde0c11679ec096dbd1aa2ced0b9e6196
21thCenturyBoy <2524755668@qq.com>
2025-07-16 23:25:46 +0800

    查找问题

--- Assets/Test/TestAI/Move/Kinematic/Steering_Collison_Avoidance.cs
+++ Assets/Test/TestAI/Move/Kinematic/Steering_Collison_Avoidance.cs
@@ -1,4 +1,4 @@
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
+using System.Collections;
 using UnityEngine;
 
 namespace TestAI.Move.Kinematic
@@ -12,17 +12,18 @@ namespace TestAI.Move.Kinematic
         public IKinematicEntity[] targetEntitys;
 
         [AIParam_Float("最大加速度")]
-        public float maxAcceleration = 10f; // 最大加速度
+        public float maxAcceleration = -10f; // 最大加速度
 
         [AIParam_Float("角色的碰撞半径")]
-        public float radius = 0.5f; //碰撞半径
+        public float radius = 1f; //碰撞半径
+
 
         public virtual SteeringOutput Separation()
         {
             var res = new SteeringOutput();
 
             //找到最接近的碰撞目标
-            var shortestTime = 0f;
+            var shortestTime = float.MaxValue;
 
             //存储碰撞目标相关数据
             IKinematicEntity firstTarget = null;
@@ -41,15 +42,21 @@ namespace TestAI.Move.Kinematic
                 var relativeVel = targetEntity.Velocity - currentEntity.Velocity;
                 var relativeSpeed = relativeVel.magnitude;//相对速度长度
 
-                
-                var timeToCollison = Vector3.Dot(relativePos, relativeVel) / (relativeSpeed * relativeSpeed);
+
+                //Vector3.Dot(relativePos, relativeVel) <0  两物体正在接近
+                //Vector3.Dot(relativePos, relativeVel) >0 两物体正在远离
+
+                if (relativeSpeed < 0.001f) continue; // 避免除零错误
+
+                var timeToCollison = Vector3.Dot(relativePos, relativeVel) / (relativeSpeed * relativeSpeed);//计算碰撞时间
+
+                if (timeToCollison < 0f) continue;
 
                 //检查是否将要完全碰撞
                 var distance = relativePos.magnitude;
                 var minSeparation = distance - relativeSpeed * timeToCollison;
 
                 if (minSeparation > 2 * radius) continue;
-
 
                 //检查是否最短时间
                 if (timeToCollison > 0 && timeToCollison < shortestTime)
@@ -85,7 +92,7 @@ namespace TestAI.Move.Kinematic
             }
 
             //躲避目标
-            res.Linear = relativePosCompute.normalized * maxAcceleration;
+            res.Linear =  relativePosCompute.normalized * maxAcceleration;
             res.Angular = 0;
 
             return res;
@@ -93,10 +100,6 @@ namespace TestAI.Move.Kinematic
         protected override void OnFixedUpdate()
         {
             var res = Separation();
-            if (currentEntity.Velocity == Vector3.zero)
-            {
-                currentEntity.Velocity = Vector3.forward;
-            }
             currentEntity.FixedUpdate(res, FixedDeltaTime);
         }
 
@@ -121,13 +124,23 @@ namespace TestAI.Move.Kinematic
                 item.SetColor(Color.red);
                 item.AllowDrag(true);
                 item.AutoMove(true);
+                item.gameObject.name = "Target_" + i;
                 targetEntitys[i] = item;
             }
 
-            currentEntity = UtilsTool.CreateNavigation_AI();
+            Navigation_AI_Item current = UtilsTool.CreateNavigation_AI() as Navigation_AI_Item;
+            currentEntity = current;
             StaticStae stae = new StaticStae();
             currentEntity.SetStaticStae(stae);
             currentEntity.SetColor(Color.green);
+
+            current.StartCoroutine(WaitForInitialization(current));
+        }
+
+        private IEnumerator WaitForInitialization(Navigation_AI_Item current)
+        {
+            yield return new WaitForSeconds(1f);
+            current.Velocity = Vector3.forward * 5f; // 设置初始速度
         }
 
         protected override void OnStop()
